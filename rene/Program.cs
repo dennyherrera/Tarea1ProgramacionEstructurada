82c9ec45c3e4da7106ed010214c84638973b0eac
Ids Ids <ids@unicaescri.onmicrosoft.com>
2019-07-19 11:09:48 -0600

    primer cambio

---
+++ rene/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rene
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double h, b;
+
+            Console.WriteLine(" ingese la altura: ");
+            h = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ingese la base: ");
+            b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(calcular(h, b));
+        }
+        static double calcular(double h, double b)
+        {
+
+            double respuesta = h * b;
+
+            return respuesta;
+        }
+    }
+}
