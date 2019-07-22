f80cc28af56b89a1b7d494cc4ab1427bb4781733
Ids Ids <ids@unicaescri.onmicrosoft.com>
2019-07-22 11:42:29 -0600

    Hice Cambios, Area de rectangulo, sin error

--- Tarea1ProgramacionEstructurada/Program.cs
+++ Tarea1ProgramacionEstructurada/Program.cs
@@ -10,7 +10,28 @@ namespace Tarea1ProgramacionEstructurada
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+             double b, h;
+
+            Console.Write("Ingrese Base: ");
+            b = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la Altura: ");
+            h = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine($"El Area de el rectangulo es: {CalcularArea(b, h)} ");
+
+            Console.ReadLine();
+
+        }
+        static double CalcularArea(double b, double h)
+        {
+            double respuesta = h * b;
+            return respuesta;
+
+
+        }
         }
     }
 }
